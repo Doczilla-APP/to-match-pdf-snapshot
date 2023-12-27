6004dd70179ca56a33dfdfbc37e662c1b669adcb
Tycho Bokdam <tycho@palmtreecoding.com>
2023-12-27 20:49:06 +0100

    ci: Remove `v` prefix for package.json version

--- update-package-json.js
+++ update-package-json.js
@@ -2,7 +2,7 @@ const fs = require('node:fs')
 
 exports.preTagGeneration = (tag) => {
   const packageJson = require('./package.json')
-  packageJson.version = tag
+  packageJson.version = tag.replace('v', '')
 
   fs.writeFileSync('./package.json', JSON.stringify(packageJson))
 
