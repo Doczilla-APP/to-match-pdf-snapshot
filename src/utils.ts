055ce43fac5a8c397325a0386b8e7cf3aa6b7e59
Tycho Bokdam <tycho@palmtreecoding.com>
2023-12-27 20:50:29 +0100

    feat: Empty change
    BREAKING CHANGE: Release v1

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-import { existsSync, statSync, writeFileSync } from 'fs'
+import { existsSync, statSync, writeFileSync } from 'node:fs'
 
 export function sameFileSize(buffer: Buffer, filePath: string, update: 'all' | 'none' | 'missing'): boolean {
   if (!existsSync(filePath) || update === 'all' || update === 'missing') {
