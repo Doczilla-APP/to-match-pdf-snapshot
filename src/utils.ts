9c8012c3fe15dd43063635e2629a7131ca389981
Tycho Bokdam <tycho@palmtreecoding.com>
2023-12-27 21:27:38 +0100

    fix: Fixed PDFs always being written

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import { existsSync, statSync, writeFileSync } from 'node:fs'
 
 export function sameFileSize(buffer: Buffer, filePath: string, update: 'all' | 'none' | 'missing'): boolean {
-  if (!existsSync(filePath) || update === 'all' || update === 'missing') {
+  if (update === 'all' || (!existsSync(filePath) && update === 'missing')) {
     writeFileSync(filePath, buffer)
 
     return true
