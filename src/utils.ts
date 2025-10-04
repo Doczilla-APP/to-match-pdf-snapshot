a55b5ebd29137e15fd7deec0f42280b88f10727e
Tycho Bokdam <tycho@palmtreecoding.com>
2025-10-04 11:39:18 +0200

    chore: Update dependencies
    - Bumped `@playwright/test` to v1.55.1 for the latest features and fixes.
    - Updated `node-addon-api` to v8.5.0 and `node-gyp` to v11.4.2 for enhanced compatibility.
    - Upgraded `eslint-plugin-import` to v2.32.0 and `ts-jest` to v29.4.4.
    - Adjusted other dependencies like `typescript` to v5.9 for improved functionality.

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 import { existsSync, statSync, writeFileSync } from 'node:fs'
 
-export function sameFileSize(buffer: Buffer, filePath: string, update: 'all' | 'none' | 'missing'): boolean {
+export function sameFileSize(buffer: Buffer, filePath: string, update: 'all' | 'changed' | 'missing' | 'none'): boolean {
   if (update === 'all' || !existsSync(filePath)) {
     writeFileSync(filePath, buffer)
 
