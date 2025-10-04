0f4dca7391bb45f1e23187bbe95ed57a4a06a142
Tycho Bokdam <tycho@palmtreecoding.com>
2025-10-04 11:39:09 +0200

    fix: Remove redundant condition in `sameFileSize` function
    - Simplified logic by removing unnecessary check for `update === 'missing'`.
    - Ensures better readability and clarity in condition handling.

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import { existsSync, statSync, writeFileSync } from 'node:fs'
 
 export function sameFileSize(buffer: Buffer, filePath: string, update: 'all' | 'none' | 'missing'): boolean {
-  if (update === 'all' || (!existsSync(filePath) && update === 'missing')) {
+  if (update === 'all' || !existsSync(filePath)) {
     writeFileSync(filePath, buffer)
 
     return true
